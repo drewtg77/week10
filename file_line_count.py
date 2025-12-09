6bdd01a297f310ef06fe0c81d0aa490f72cfbfac
drewtg77 <xmjv0622@leeds.ac.uk>
2025-12-09 19:34:47 +0000

    git add -A git commit -m git push

--- file_line_count.py
+++ file_line_count.py
@@ -1,6 +1,6 @@
 
-# Name:
-# Username: 
+# Name: Drew Girton
+# Username: xmjv0622
 
 import sys
 
@@ -10,19 +10,23 @@ import sys
 # You must use the error messages precisely as defied below.
 
 # filename is a command line argument 
-file_name = ""
-
 # Error message: "Usage: python file_line_count.py <filename.txt>"
-
 # Open the file and read its content
-
 # Error message: "File not found: python file_line_count.py {file_name}"
-
 # count number of lines that are not blank
-number_of_lines = 0
-
 # success - report the number of lines
-
 # Success message: "{file_name} has {number_of_lines} lines"
-
 # Test on the 'text_example.txt' file. The answer should be 4.
+file_name = "text_example.txt"
+if file_name == '':
+    print("Usage: python file_line_count.py <filename.txt>")
+try:
+    number_of_lines = 0
+    with open(file_name) as file:
+        for x in file:
+            if x !='\n':
+                number_of_lines = number_of_lines + 1
+    print(f"{file_name} has {number_of_lines} lines")
+    file.close()
+except FileNotFoundError:
+    print(f"File not found: python file_line_count.py {file_name}")
--- ziplist.py
+++ ziplist.py
@@ -1,6 +1,6 @@
 
-# Name:
-# Username:
+# Name: Drew Girton
+# Username: xmjv0622
 
 # Lists contents of a zip archive
 # usage: python ziplist.py <zipfile>
@@ -11,18 +11,29 @@
 
 import sys
 #from zipfile import ...   # tools in this module can be used
-
+import zipfile
 # filename is a command line argument 
-file_name = ""
-
 # Error message: "Usage: python ziplist.py <filename.zip>"
-
 # Error message: "File not found: python ziplist.py {file_name}"
-
 # Error message: "Bad zip file: python ziplist.py {file_name}"
 # Hint: There is an exception for this defined in the zipfile module
 
 # Use zipfile methods to list the contents of the zip file.
+
+file_name = "zip_example.zip"
+
+if file_name == '':
+    print("Usage: python ziplist.py <filename.zip>")
+try:
+    with zipfile.ZipFile(file_name) as file:
+        print(file.namelist())
+    file.close()
+
+except FileNotFoundError:
+    print(f"File not found: python ziplist.py {file_name}")
+except zipfile.BadZipFile:
+    print(f"Bad zip file: python ziplist.py {file_name}")
+
 # Test your script on the zip_example.zip file. The response should be:
 # README.md
 # cmd_line.py
